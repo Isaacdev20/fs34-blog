ea616a3f972e2122f0020cb6139bfbdc8cd0b7d7
Isaac <sousaisaac58@gmail.com>
2024-06-22 11:55:30 -0300

    primeiras mudancas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import { useState } from 'react'
+
+function Header({children}){
+   return (
+    <header className='bg-blue-300'>
+      <nav className='mx-auto flex items-center justify-between p-6 '>
+        {children}
+      </nav>
+    </header>
+    )
+}
+
+function HeaderLink ({href,children}){
+  return (
+    <a href={href} className="font-semibold leading-6 ml-5 text-white">{children}</a>
+  )
+}
 
 function App() {
   return (
-    <h1 className="text-5xl font-bold underline">
-    Hello world!
-    </h1>    
-  )
+      <>
+        <Header>
+            <HeaderLink
+            href="https://company">
+              Company
+            </HeaderLink>
+
+            <HeaderLink
+            href="https://company">
+              Bola
+            </HeaderLink>
+            
+            <HeaderLink
+              
+            >Comercio</HeaderLink>
+          
+        </Header>
+      </>
+    )
 }
 
 export default App
